c663af20cb7f0ba9d30f31056d9c4a4c05ff1d66
Vanesmen <46038353+Vanesmen@users.noreply.github.com>
2019-07-30 23:13:07 +0700

    ДДР

---
+++ ddr/js/script.js
@@ -0,0 +1,81 @@
+let btnS = document.querySelectorAll("div.btn");
+let lessonItem = document.querySelectorAll(".lesson-item");
+console.log(btnS);
+console.log(lessonItem);
+
+btnS[0].addEventListener('click', function() {
+    for (i = 0; i < lessonItem.length; i++) {
+        lessonItem[i].style.display = "none"
+    }
+    lessonItem[0].style.display = "flex"
+})
+
+btnS[1].addEventListener('click', function() {
+    for (i = 0; i < lessonItem.length; i++) {
+        lessonItem[i].style.display = "none";
+    }
+    lessonItem[1].style.display = "flex"
+})
+
+btnS[2].addEventListener('click', function() {
+    for (i = 0; i < lessonItem.length; i++) {
+        lessonItem[i].style.display = "none";
+    }
+    lessonItem[2].style.display = "flex"
+})
+
+btnS[3].addEventListener('click', function() {
+    for (i = 0; i < lessonItem.length; i++) {
+        lessonItem[i].style.display = "none";
+    }
+    lessonItem[3].style.display = "flex"
+})
+
+btnS[4].addEventListener('click', function() {
+    for (i = 0; i < lessonItem.length; i++) {
+        lessonItem[i].style.display = "none";
+    }
+    lessonItem[4].style.display = "flex"
+})
+
+btnS[5].addEventListener('click', function() {
+    for (i = 0; i < lessonItem.length; i++) {
+        lessonItem[i].style.display = "none";
+    }
+    lessonItem[5].style.display = "flex"
+})
+
+btnS[6].addEventListener('click', function() {
+    for (i = 0; i < lessonItem.length; i++) {
+        lessonItem[i].style.display = "none";
+    }
+    lessonItem[6].style.display = "flex"
+})
+
+btnS[7].addEventListener('click', function() {
+    for (i = 0; i < lessonItem.length; i++) {
+        lessonItem[i].style.display = "none";
+    }
+    lessonItem[7].style.display = "flex"
+})
+
+btnS[8].addEventListener('click', function() {
+    for (i = 0; i < lessonItem.length; i++) {
+        lessonItem[i].style.display = "none";
+    }
+    lessonItem[8].style.display = "flex"
+})
+
+btnS[9].addEventListener('click', function() {
+    for (i = 0; i < lessonItem.length; i++) {
+        lessonItem[i].style.display = "none";
+    }
+    lessonItem[9].style.display = "flex"
+})
+
+btnS[10].addEventListener('click', function() {
+    for (i = 0; i < lessonItem.length; i++) {
+        lessonItem[i].style.display = "none";
+    }
+    lessonItem[10].style.display = "flex"
+})
